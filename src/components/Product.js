b4cbac262c49a70be1db172410a3342366161def
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-04-12 22:22:03 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,28 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class  Product extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.name}</p>
+        <p>{this.props.producer}</p>
+        <p>{this.props.hasWatermark}</p>
+        <p>{this.props.color}</p>
+      </div>
+    )
+  }
+}
+
+Order.defaultProps = {
+  hasWatermark: false,
+}
+
+Order.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.
+  weight: 
+}
